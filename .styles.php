99ed708cfa1540eb5ff9d8944fa71ece87bf200d
Виктор <63960097+webnav697@users.noreply.github.com>
2021-01-18 16:12:50 +0200

    Заливка файлов
    Заливка файлов сайта

---
+++ .styles.php
@@ -0,0 +1,275 @@
+<?
+return array (
+	'text-backlight' => 'Цветной блок текста',
+	'text-backlight' =>
+        array (
+            'tag' => 'div',
+            'title' => 'Цветной блок текста',
+            'html' => '<div class="text-backlight">Блок цветной</div>',
+            'section' => 'block',
+        ),
+	'kquote' =>
+        array (
+            'tag' => 'p',
+            'title' => 'Цитируемый абзац',
+            'html' => '<span style="border-left: 2px solid #ab9f5c;font-style: italic;color: #ab9f5c;padding:9px 10px;">Цитируемый абзац</span>',
+            'section' => 'quote',
+        ),
+    'bxe-quote' =>
+        array (
+            'tag' => 'blockquote',
+            'title' => 'Обычная цитата',
+            'section' => 'quote',
+        ),
+    'blockquote' =>
+        array (
+            'tag' => 'blockquote',
+            'title' => 'Цитируемый абзац',
+            'section' => 'quote',
+        ),
+    'lead' =>
+        array (
+            'tag' => 'p',
+            'title' => 'Цитируемый абзац, голубой',
+            'section' => 'quote',
+        ),
+    'piksell-color' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Цвет синий',
+            'html' => '<span style="color: #007B86;">Синий цвет</span>',
+            'section' => 'text',
+        ),
+    'bg-primary' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Синий',
+            'html' => '<span style="background: #60aadb;color: #fff; padding: 4px;">Синяя подложка</span>',
+            'section' => 'text',
+        ),
+    'bg-success' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Зеленый',
+            'html' => '<span style="background: #64ba4e;color: #fff; padding: 4px;">Зеленая подложка</span>',
+            'section' => 'text',
+        ),
+    'bg-info' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Голубой',
+            'html' => '<span style="background: #9bc6dd;color: #fff; padding: 4px;">Голубая подложка</span>',
+            'section' => 'text',
+        ),
+    'bg-warning' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Желтый',
+            'html' => '<span style="background: #fec139;color: #fff; padding: 4px;">Желтая подложка</span>',
+            'section' => 'text',
+        ),
+    'bg-danger' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Розовый',
+            'html' => '<span style="background: #f2dede;color: #000; padding: 4px;">Розовая подложка</span>',
+            'section' => 'text',
+        ),
+    'highlight' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Выделение желтое',
+            'html' => '<span style="background: #ffffaa; padding: 1px 2px;">Желтое выделение</span>',
+            'section' => 'text',
+        ),
+    'code' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Выделение',
+            'html' => '<span style="background-color: #EDEDED; padding: 2px 5px; border-radius: 2px 2px 2px 2px;">Серое выделение</span>',
+            'section' => 'text',
+        ),
+    'bloktel' =>
+        array (
+            'tag' => 'p',
+            'title' => 'Выделяем абзац',
+            'html' => '<div style="padding: 10px;color: #fff;background-color: #4A4A4A;">Выделяем абзац</div>',
+            'section' => 'text',
+        ),
+    'first' =>
+        array (
+            'tag' => 'p',
+            'title' => 'Увеличиваем первую букву',
+            'html' => '<span style="float: left; font-size: 20px; padding: 0px; margin: 3px; line-height: 0.35em;">В</span>ыделение',
+            'section' => 'text',
+        ),
+    'dropcap-alt' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Выделяем вторую букву в абзаце',
+            'html' => '<span style="float: left; background: #B7D9E2; color: #fff; font-size: 1.2em; padding: 2px 5px; margin-right: 5px; -webkit-border-radius: 2px;  -moz-border-radius: 2px;  border-radius: 2px;">В</span>ыделение -p-',
+            'section' => 'text',
+        ),
+    'dropcap-alt-h3' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Выделяем вторую букву в заголовке',
+            'html' => '<span style="float: left; background: #B7D9E2; color: #fff; font-size: 1.2em; padding: 4px 5px; margin-right: 5px; -webkit-border-radius: 2px;  -moz-border-radius: 2px;  border-radius: 2px;">В</span>ыделение -h3-',
+            'section' => 'text',
+        ),
+    'fancyamp' =>
+        array (
+            'tag' => 'span',
+            'title' => 'Увеличение фразы',
+            'html' => '<span style="font-family: \'Gabriela\', \'Trebuchet MS\', serif; font-size: 1.6em;">Увеличение</span>',
+            'section' => 'text',
+        ),
+    'notice' =>
+        array (
+            'tag' => 'div',
+            'title' => 'Выделяемый блок',
+            'html' => '<div style="display: block; padding: 12px 20px; background: #fff5af; color: #a79b44; text-shadow: 1px 1px 0 #fff; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;">Блок</div>',
+            'section' => 'block',
+        ),
+    'notice-html' =>
+        array (
+            'tag' => 'div',
+            'title' => 'Выделяемый блок html',
+            'html' => '<div style="display: block; padding: 12px 20px; background: #49A744; color: #fff; text-shadow: 1px 1px 0 #4a4444; -webkit-border-radius: 4px 4px 0 0px; -moz-border-radius: 4px 4px 0 0px; border-radius: 4px 4px 0 0px;">Блок -html-</div>',
+            'section' => 'block',
+        ),
+    'notice-css' =>
+        array (
+            'tag' => 'div',
+            'title' => 'Выделяемый блок css',
+            'html' => '<div style="display: block; padding: 12px 20px; background: #449da7; color: #fff; text-shadow: 1px 1px 0 #4a4444; -webkit-border-radius: 4px 4px 0 0px; -moz-border-radius: 4px 4px 0 0px; border-radius: 4px 4px 0 0px;">Блок -css-</div>',
+            'section' => 'block',
+        ),
+    'notice-js' =>
+        array (
+            'tag' => 'div',
+            'title' => 'Выделяемый блок js',
+            'html' => '<div style="display: block; padding: 12px 20px; background: #a74491; color: #fff; text-shadow: 1px 1px 0 #4a4444; -webkit-border-radius: 4px 4px 0 0px; -moz-border-radius: 4px 4px 0 0px; border-radius: 4px 4px 0 0px;">Блок -js-</div>',
+            'section' => 'block',
+        ),
+    'licon' =>
+        array (
+            'tag' => 'ul',
+            'title' => 'Красный список',
+            'html' => '<ul style="font: 14px Verdana, sans-serif;color: #A00000;font-style: italic;margin-left: 25px;margin-bottom: 6px;padding: 2px 0 3px 10px;"><li>Красный список</li></ul>',
+            'section' => 'list',
+        ),
+    'ul' =>
+        array (
+            'title' => 'Стандартный',
+            'html' => '<UL style="margin: 0; padding:0 0 0 10px;"><LI>Стандартный список</LI></UL>',
+            'section' => 'list',
+            'tag' => 'UL',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-check-square' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "флажок"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-check-square" style="color: #0083d1;margin-right: 6px"></i>Список "флажок"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-check' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "птичка"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-check" style="color: #0083d1;margin-right: 6px"></i>Список "птичка"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-minus' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "минус"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-minus" style="color: #0083d1;margin-right: 6px"></i>Список "минус"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-map-marker' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "маркер карты"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-map-marker" style="color: #0083d1;margin-right: 6px"></i>Список "маркер карты"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-rocket' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "ракета"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-rocket" style="color: #0083d1;margin-right: 6px"></i>Список "ракета"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-paw' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "след"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-paw" style="color: #0083d1;margin-right: 6px"></i>Список "след"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-paper-plane' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "самолетик"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-paper-plane" style="color: #0083d1;margin-right: 6px"></i>Список "самолетик"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-certificate' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "печать"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-certificate" style="color: #0083d1;margin-right: 6px"></i>Список "печать"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-flag' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "флаг"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-flag" style="color: #0083d1;margin-right: 6px"></i>Список "флаг"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-bomb' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "бомбочка"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-bomb" style="color: #0083d1;margin-right: 6px"></i>Список "бомбочка"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-close' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "крестик"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-close" style="color: #0083d1;margin-right: 6px"></i>Список "крестик"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-star' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "звезда"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-star" style="color: #0083d1;margin-right: 6px"></i>Список "звезда"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-times-circle' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "крестик в круге"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-times-circle" style="color: #0083d1;margin-right: 6px"></i>Список "крестик в круге"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-caret-square-o-right' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "видео"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-caret-square-o-right" style="color: #0083d1;margin-right: 6px"></i>Список "видео"</li><ul>',
+        ),
+    'bxe-list bxe-lis-blue~~fa fa-heart' =>
+        array (
+            'section' => 'list',
+            'tag' => 'UL',
+            'title' => 'Список "сердечко"',
+            'html' => '<ul  style="margin: 0; padding:0; list-style: none;"><li><i class="fa fa-heart" style="color: #0083d1;margin-right: 6px"></i>Список "сердечко"</li><ul>',
+        ),
+);
+?>
---
+++ description.php
@@ -0,0 +1,13 @@
+<?
+$arTemplate = array (
+  'NAME' => 'Мебель ',
+  'DESCRIPTION' => 'Шаблон сайта мебельной фабрики',
+  'SORT' => 1,
+	'TYPE' => '',
+	'EDITOR_STYLES' => 
+  array (
+    0 => '/bitrix/css/main/bootstrap.css',
+    1 => '/bitrix/css/main/font-awesome.css',
+  ),
+);
+?>
