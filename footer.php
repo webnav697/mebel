f2829e942ad8ccb9aae15103e51784f840267639
webnav697 <webnav697@gmail.com>
2021-01-19 15:17:49 +0200

    Вёрстку 3-х блоков поместил в вкл обл. для раздела

--- footer.php
+++ footer.php
@@ -31,31 +31,50 @@
 								</ul>
 							</li>
 						</ul>
-					</div>
-					<div class="sb_event">
-						<div class="sb_event_header"><h4>Ближайшие события</h4></div>
-						<p><a href="">29 августа 2012, Москва</a></p>
-						<p>Семинар производителей мебели России и СНГ, Обсуждение тенденций.</p>
-					</div>
-					<div class="sb_action">
-						<a href=""><img src="<?=SITE_TEMPLATE_PATH?>/content/11.png" alt=""/></a>
-						<h4>Акция</h4>
-						<h5><a href="">Мебельная полка всего за 560 Р</a></h5>
-						<a href="" class="sb_action_more">Подробнее &rarr;</a>
-					</div>
-					<div class="sb_reviewed">
-							<img src="<?=SITE_TEMPLATE_PATH?>/content/8.png" class="sb_rw_avatar" alt=""/>
-							<span class="sb_rw_name">Сергей Антонов</span>
-							<span class="sb_rw_job">Руководитель финансового отдела “Банк+”</span>
-							<p>“Покупал офисные стулья и столы, остался очень доволен! Низкие цены, быстрая доставка, обслуживание на высоте! Спасибо!”</p>
-							<div class="clearboth"></div>
-							<div class="sb_rw_arrow"></div>
-					</div>
-				</div>
+					</div>		
+
+<!--Включаемая область только для раздела,-->
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	"", 
+	array(
+		"AREA_FILE_RECURSIVE" => "Y",
+		"AREA_FILE_SHOW" => "sect",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"COMPONENT_TEMPLATE" => ""
+	),
+	false
+);?>
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_RECURSIVE" => "Y",
+		"AREA_FILE_SHOW" => "sect",
+		"AREA_FILE_SUFFIX" => "inc2",
+		"EDIT_TEMPLATE" => ""
+	)
+);?>
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_RECURSIVE" => "Y",
+		"AREA_FILE_SHOW" => "sect",
+		"AREA_FILE_SUFFIX" => "inc3",
+		"EDIT_TEMPLATE" => ""
+	)
+);?>
+<!-- / end Включаемая область только для раздела  -->
 				<div class="clearboth"></div>
 			</div>
 		</div>
 			<?}?>
+					<div class="clearboth"></div>
+		</div>	
 		<div class="ft_footer">
 			<div class="ft_container">
 				<div class="ft_about">
