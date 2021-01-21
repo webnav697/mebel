0f9ec2b5a888bd9e9123a5bbae1d42aa0467fe1c
webnav697 <webnav697@gmail.com>
2021-01-21 17:15:11 +0200

    тег br добавил

--- components/bitrix/news/news_mebel/bitrix/news.detail/.default/template.php
+++ components/bitrix/news/news_mebel/bitrix/news.detail/.default/template.php
@@ -15,16 +15,17 @@ $this->setFrameMode(true);
 
 	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
 	<div class="main_title">
-		<p class="title"><?=$arResult["NAME"]?></p>
-		<span class="main_date"><?$arResult["DISPLAY_ACTIVE_FROM"];?></span>
+		<p class="title"><?=$arResult["NAME"]?></p><br>
+		<span class="main_date"><?=$arResult["DISPLAY_ACTIVE_FROM"];?></span>
 	</div>
 	<?endif;?>
 
 	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
 		<img 	src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
 			alt="<?=$arResult["NAME"]?>" 	title="<?=$arResult["NAME"]?>"
-			"align="left"
+			align="left"
 			/>
 	<?endif?>
 	<p><?echo $arResult["DETAIL_TEXT"];?></p>
+
 
