3624e32792d88bde88ae27abcee2a66f59a239f8
webnav697 <webnav697@gmail.com>
2021-01-22 18:56:46 +0200

    Вёрстка отзывов в компоненте список новостей

---
+++ components/bitrix/news.list/reviews_clients/template.php
@@ -0,0 +1,49 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<?if($arParams["DISPLAY_TOP_PAGER"]):?>
+	<br /><?=$arResult["NAV_STRING"]?>
+<?endif;?>
+
+<?foreach($arResult["ITEMS"] as $arItem):?>
+<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+<div class="review-block" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+	<div class="review-text">
+	
+		<div class="review-block-title">
+			<span class="review-block-name">
+				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a>
+			</span>
+				<span class="review-block-description">
+					<?=$arItem["PROPERTIES"]["POST"]["VALUE"]?>
+				</span>
+		</div>
+		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+		<div class="review-text-cont">
+			<?=$arItem["PREVIEW_TEXT"];?>
+		</div>
+		<?endif;?>
+				
+	</div>
+	<div class="review-img-wrap">
+	<a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
+		<img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?> "title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+					/>
+	</a>
+	</div>
+</div>
+<?endforeach;?>
