554a7d4083542279de01509992641bda5ec84067
webnav697 <webnav697@gmail.com>
2021-01-22 20:13:07 +0200

    Изменения  описания компонентов в файлах  lang

--- components/bitrix/menu/bootom-1/lang/ru/.description.php
+++ components/bitrix/menu/bootom-1/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS ['MENU_DOT_DEFAULT_NAME'] = "Вертикальное меню по умолчанию";
-$MESS ['MENU_DOT_DEFAULT_DESC'] = "Вертикальное меню по умолчанию";
+$MESS ['MENU_DOT_DEFAULT_NAME'] = "Нижнее меню 1";
+$MESS ['MENU_DOT_DEFAULT_DESC'] = "Нижнее меню 1";
 ?>
--- components/bitrix/menu/bootom-2/lang/en/.description.php
+++ components/bitrix/menu/bootom-2/lang/en/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS ['MENU_DOT_DEFAULT_NAME'] = "Default vertical menu";
-$MESS ['MENU_DOT_DEFAULT_DESC'] = "Default vertical menu";
+$MESS ['MENU_DOT_DEFAULT_NAME'] = "Bottom menu 2";
+$MESS ['MENU_DOT_DEFAULT_DESC'] = "Bottom menu 2";
 ?>
