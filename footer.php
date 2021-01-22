97558ca18023ec6b16b3fb7657efbc95e08756c1
webnav697 <webnav697@gmail.com>
2021-01-22 19:41:16 +0200

    Вывод компонента меню нижнего левого

--- footer.php
+++ footer.php
@@ -76,12 +76,22 @@
 			<div class="ft_container">
 				<div class="ft_about">
 					<h4>О магазине</h4>
-					<ul>
-						<li><a href="">Отзывы</a></li>
-						<li><a href="">Контакты</a></li>
-						<li><a href="">Руководство</a></li>
-						<li><a href="">История</a></li>
-					</ul>
+						<?$APPLICATION->IncludeComponent(
+							"bitrix:menu",
+							"bootom-1",
+							Array(
+								"ALLOW_MULTI_SELECT" => "N",
+								"CHILD_MENU_TYPE" => "bottom",
+								"DELAY" => "N",
+								"MAX_LEVEL" => "1",
+								"MENU_CACHE_GET_VARS" => array(0=>"",),
+								"MENU_CACHE_TIME" => "3600",
+								"MENU_CACHE_TYPE" => "N",
+								"MENU_CACHE_USE_GROUPS" => "Y",
+								"ROOT_MENU_TYPE" => "bottom",
+								"USE_EXT" => "N"
+							)
+						);?>
 				</div>
 				<div class="ft_catalog">
 					<h4>Каталог товаров</h4>
