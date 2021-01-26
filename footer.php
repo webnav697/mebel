dac69c03c39c591fe53f1907d14c50da71ce6b18
webnav697 <webnav697@gmail.com>
2021-01-26 14:41:26 +0200

    es

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 		"MENU_CACHE_GET_VARS" => array(
 		),
 		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TYPE" => "N",
 		"MENU_CACHE_USE_GROUPS" => "Y",
 		"ROOT_MENU_TYPE" => "left",
 		"USE_EXT" => "Y",
